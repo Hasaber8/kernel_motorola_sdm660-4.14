sonyxperiadev/kernel

gpu: msm: kgsl_iommu: Don't program SMMU aperture on SDM630/660
Like MSM8998, SDM630/660 do support the SCM call for service
CP_SMMU_APERTURE_ID, but the TZ firmware disallows programming
the SMMU apearture for at least the KGSL user context.

drivers/gpu/msm/kgsl_iommu.c
@@ -1221,7 +1221,9 @@ static int _init_global_pt(struct kgsl_mmu *mmu, struct kgsl_pagetable *pt)
 	}
 
 	if (!MMU_FEATURE(mmu, KGSL_MMU_GLOBAL_PAGETABLE) &&
-		!of_machine_is_compatible("qcom,msm8998") &&
+		(!of_machine_is_compatible("qcom,msm8998") &&
+		 !of_machine_is_compatible("qcom,sdm660") &&
+		 !of_machine_is_compatible("qcom,sdm630")) &&
 		scm_is_call_available(SCM_SVC_MP, CP_SMMU_APERTURE_ID)) {
 		ret = program_smmu_aperture(cb_num, CP_APERTURE_REG);
 		if (ret) {

Hasaber8/kernel_motorola_sdm660-4.14

Change-Id: I639d51a4b8f68d9ec1cd98703e444bdeb034fc96

drivers/gpu/msm/kgsl_iommu.c
@@ -1284,7 +1284,8 @@ static int _init_global_pt(struct kgsl_mmu *mmu, struct kgsl_pagetable *pt)
 	}
 	context_bank_number = cb_num;
 	if (!MMU_FEATURE(mmu, KGSL_MMU_GLOBAL_PAGETABLE) &&
-		!of_machine_is_compatible("qcom,sdm660") &&
+		(!of_machine_is_compatible("qcom,sdm660") &&
+		 !of_machine_is_compatible("qcom,sdm630")) &&
 		scm_is_call_available(SCM_SVC_MP, CP_SMMU_APERTURE_ID)) {
 		ret = kgsl_program_smmu_aperture();
 		if (ret) {